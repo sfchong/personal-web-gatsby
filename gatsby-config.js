d5d36c002b62ff79b2ed00cde3c853980c91cfb1
SF Chong <soonfook11@hotmail.com>
2021-04-09 23:09:13 +0800

    add gatsby-remark-images plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -11,6 +12,26 @@ module.exports = {
         name: `markdowns`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdowns/images`,
+        name: `markdownImages`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+              wrapperStyle: `margin-bottom:20px`,
+            },
+          },
+        ],
+      },
+    },
   ],
-};
+}
