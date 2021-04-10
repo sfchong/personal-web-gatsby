83e64148fe297c4374379b595dbb0dff5476597f
SF Chong <soonfook11@hotmail.com>
2021-04-11 03:30:39 +0800

    add title style

--- src/templates/blogContentTemplate.js
+++ src/templates/blogContentTemplate.js
@@ -2,6 +2,8 @@ import React from "react"
 import { graphql } from "gatsby"
 import Header from "../components/header"
 import Footer from "../components/footer"
+import { FaRegCalendarAlt } from "react-icons/fa"
+import { FaRegClock } from "react-icons/fa"
 
 export default function Template({ data }) {
   const { markdownRemark } = data
@@ -11,11 +13,24 @@ export default function Template({ data }) {
       <title>{frontmatter.title}</title>
       <Header />
       <div className="content-wrapper">
-        <h1>{frontmatter.title}</h1>
-        <h2>
-          {frontmatter.date} ({frontmatter.fromNow})
-        </h2>
-        <p>{frontmatter.tags.join()}</p>
+        <div className="blog-top-wrapper">
+          <h1 className="blog-title">{frontmatter.title}</h1>
+          <div className="blog-date-container">
+            <span className="blog-date">
+              <FaRegCalendarAlt className="blog-icon" />
+              <span>{frontmatter.date}</span>
+            </span>
+            <span>
+              <FaRegClock className="blog-icon" />
+              <span>{frontmatter.fromNow}</span>
+            </span>
+          </div>
+          <div>
+            {frontmatter.tags?.map((tag) => (
+              <span className="blog-tag">{tag}</span>
+            ))}
+          </div>
+        </div>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
