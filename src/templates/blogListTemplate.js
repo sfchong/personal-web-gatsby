9a04b7062a146d803c6f23d9d82c262166c69627
Adrian Chong <adrian@adrianchongsf.com>
2021-04-23 01:51:05 +0800

    add blog preview text

--- src/templates/blogListTemplate.js
+++ src/templates/blogListTemplate.js
@@ -11,11 +11,12 @@ export default function Template({ pageContext }) {
     <div className="content-wrapper">
       <title>Blog</title>
       <section className="blog-list-wrapper">
-        {pageContext.nodes?.map(({ id, timeToRead, frontmatter }) => (
+        {pageContext.nodes?.map(({ id, html, timeToRead, frontmatter }) => (
           <BlogList
             key={id}
             timeToRead={timeToRead}
             frontmatter={frontmatter}
+            html={html}
           />
         ))}
       </section>
@@ -23,24 +24,34 @@ export default function Template({ pageContext }) {
   )
 }
 
-const BlogList = ({ timeToRead, frontmatter }) => {
+const BlogList = ({ timeToRead, frontmatter, html }) => {
+  let htmlText = new DOMParser().parseFromString(html, "text/html")
+    .documentElement.textContent
+  htmlText = shortenHtmlString(htmlText, 250)
+
   return (
     <div className="blog-list-item-wrapper">
       <Link to={"/blog/" + frontmatter.slug}>
-        <h2 className="blog-title ">{frontmatter.title}</h2>
+        <p className="blog-title ">{frontmatter.title}</p>
       </Link>
       <div className="blog-date-container">
         <BlogDateIcon date={frontmatter.date} />
         <BlogHistoryIcon fromNow={frontmatter.fromNow} />
         <BlogTimeIcon timeToRead={timeToRead} />
       </div>
-      <div>
+      <div className="blog-tag-container">
         {frontmatter?.tags?.map((tag, index) => (
           <span key={index} className="blog-tag">
             {tag}
           </span>
         ))}
       </div>
+      <div className="blog-content-preview">{htmlText}</div>
     </div>
   )
+}
+
+function shortenHtmlString(str, maxLen, separator = " ") {
+  if (str.length <= maxLen) return str
+  return str.substr(0, str.lastIndexOf(separator, maxLen)) + " ..."
 }
